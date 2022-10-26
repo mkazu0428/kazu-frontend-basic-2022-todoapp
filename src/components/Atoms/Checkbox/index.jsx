b6f8df835259a1e5b59494a45554d5c465c8f651
mkazu0428 <k.miyahara@4nonome.com>
2022-10-26 17:32:06 +0900

    Atoms/Checkboxの作成

---
+++ src/components/Atoms/Checkbox/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import check from "../../../assets/svg/check.svg";
+import COLOR from "../../../variables/color";
+
+const Checkbox = (props) => {
+  return (
+    <CheckboxTag onClick={props.onClick}>
+      <ImgCheck src={check} />
+    </CheckboxTag>
+  );
+};
+
+const ImgCheck = styled.img`
+  width: 16px;
+  height: 16px;
+  opacity: 0;
+`;
+const CheckboxTag = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  height: 20px;
+  border: 1px solid ${COLOR.LIGHT_GRAY};
+  border-radius: 2px;
+  padding: 0;
+  background-color: transparent;
+  &:hover ${ImgCheck} {
+    opacity: 1;
+  }
+`;
+
+<Checkbox onClick={() => console.log("click")} />;
+
+export default Checkbox;
